795d84ce0f4130714e3be7cae1f53c72204b866b
imyme <imyme@meow.me>
2024-09-25 14:13:39 +0800

    bugfix

--- krita_popup/Popup.py
+++ krita_popup/Popup.py
@@ -1,12 +1,10 @@
-"""
-TODO 当前有bug，无压感！！！
-"""
 from typing import Literal, Optional
 from PyQt5.QtCore import *
-from PyQt5.QtCore import QChildEvent
+from PyQt5.QtCore import QChildEvent, Qt
 from PyQt5.QtGui import QPaintEvent, QResizeEvent
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QWidget
 
 class CrossOverlay(QWidget):
     """
@@ -58,23 +56,27 @@ class PopupItem(QWidget):
     A wrapper for widgets added onto Popup, the wrapped widget's parent will be the wrapper, used for controlling interactivity and providing helpful methods.
     """
     def __init__(self, wrapped: QWidget) -> None:
+        """
+        identifier: should be unique
+        """
         super().__init__(wrapped.parentWidget())
         self.__wrapped = wrapped
         self.__wrapped.setParent(self)
         self.__mask = QLabel(self)
         self.__mask.setMouseTracking(True)
         self.__mask.setStyleSheet("background-color: rgba(0,0,0,0);")  # 设置透明背景
+        self.__mask.setGeometry(self.rect())
         self.__mask.raise_()
+        self.__mask.show()
+
+        self.__interactive = False
 
         # add a resizeEventFilter to synchronize size
         self.__resize_event_filter = type('', (QObject,), {})()
         def eventFilter(_, a0: QObject, a1: QEvent) -> bool:
-            if a1.type() == QEvent.Resize:
+            if a1.type() in (QEvent.Resize, QEvent.Move):
                 self.__wrapped.setGeometry(self.rect())
-                if not self.is_interactive():
-                    self.__mask.setGeometry(self.rect())
-                else:
-                    self.__mask.setGeometry(0,0,0,0)
+                self.interactive = self.interactive
             return False
         self.__resize_event_filter.eventFilter = eventFilter.__get__(self.__resize_event_filter)
         self.installEventFilter(self.__resize_event_filter)
@@ -83,29 +85,111 @@ class PopupItem(QWidget):
     def wrapped(self):
         return self.__wrapped
     
-    def is_interactive(self):
-        return not self.__mask.isVisible()
-
-    def set_interactive(self, v: bool):
-        """
-        set if the wrapped widget is interactive, controlling by showing a transparent widget as mask
-        """
-        self.__mask.setVisible(not v)
-        if not v:
-            self.__mask.setGeometry(self.rect())
-        else:
+    @property
+    def interactive(self):
+        return self.__interactive
+    
+    @interactive.setter
+    def interactive(self, v: bool):
+        self.__interactive = v
+        if v:
             self.__mask.setGeometry(0, 0, 0, 0)
+        else:
+            self.__mask.setGeometry(self.rect())
 
-class RealPopup(QWidget):
+def get_screen_size(widget: QWidget):
+    return QApplication.screenAt(widget.pos()).size()
+
+class Popup(QWidget):
     """
     The Real displayed popop widget, innocent to Krita for purity, use show and hide method to toggle display
     """
 
-    def __init__(self) -> None:
+    def __init__(self, 
+                 items: list[tuple[QWidget, QRect]],
+                 /,*,
+                 screen: QScreen = None,
+                 under_cursor = True,
+                 ) -> None:
         super().__init__(None)
-        self.__under_cursor = False
-        self.__editing_mode = False
-        self.__items: list[tuple[PopupItem, QRect]] = []
+        self.__items: list[tuple[QWidget, QRect]] = []
+        
+        self.__under_cursor = under_cursor
+        self.setAttribute(Qt.WA_TranslucentBackground, True) # 透明背景，必须和无边框结合使用
+        self.setWindowFlag(Qt.FramelessWindowHint, True) # 无边框
+        self.setWindowFlag(Qt.WindowStaysOnTopHint, True) # 置顶
+        
+        self.__mask = QRegion()
+        for i in items:
+            self.add_item(*i)
+        if not screen:
+            screen = QApplication.screenAt(self.pos())
+        self.__geo = screen.geometry()
+        self.show()
+        self.hide()
+        # TODO mask
+    
+    def refresh_mask(self):
+        for item, _ in self.__items:
+            self.__mask.intersected(item.geometry())
+        self.setMask(self.__mask)
+
+    def add_item(self, widget: QWidget, relative_geo: QRect):
+        """
+        Add a widget to me
+        """
+        wrapper = PopupItem(widget)
+        wrapper.setParent(self)
+        wrapper.interactive = True
+        wrapper.setGeometry(self.__to_real_geo(relative_geo))
+        wrapper.show()
+        self.__items.append((wrapper, relative_geo))
+        self.refresh_mask()
+
+    def __to_real_geo(self, relative_geo: QRect):
+        # if not self.__under_cursor:
+        center = self.geometry().center()
+        # else:
+        #     center = QCursor.pos()
+        #     print(f'{QCursor.pos()=}')
+        res = QRect(relative_geo)
+        res.moveTopLeft(res.topLeft() + center)
+        return res
+    
+    def resizeEvent(self, a0: QResizeEvent) -> None:
+        self.setGeometry(self.__geo)
+        for item, relative_geo in self.__items:
+            item.unsetCursor()
+            item.setGeometry(self.__to_real_geo(relative_geo))
+        self.refresh_mask()
+        return super().resizeEvent(a0)
+    
+    def show(self):
+        # TODO setup mask
+        print('show')
+        if not self.__under_cursor:
+            self.setGeometry(self.__geo)
+        else:
+            geo = QRect(self.__geo)
+            geo.moveCenter(QCursor.pos())
+            self.setGeometry(geo)
+            
+        super().show()
+    def hide(self):
+        print('hide')
+        # TODO unset mask
+        super().hide()
+
+class EditingPopup(QWidget):
+    """
+    The editing popup
+    """
+    def __init__(self, items: list[tuple[QWidget, QRect]]) -> None:
+        super().__init__(None)
+        self.setAttribute(Qt.WA_TranslucentBackground, True) # 透明背景，必须和无边框结合使用
+        self.setWindowFlag(Qt.FramelessWindowHint, True) # 无边框
+        self.setWindowFlag(Qt.WindowStaysOnTopHint, True) # 置顶
+
         # delegate item's event to me
         self.__delegate_event_filter = type('', (QObject,), {})(self)
         def delegate_event_filter_fn(_, widget, event):
@@ -119,93 +203,62 @@ class RealPopup(QWidget):
         def resize_event_filter_fn(_, widget, event):
             self.__cross_line.setGeometry(self.rect())
             return False
+        self.__cross_line.setVisible(True) 
+        
         self.__resize_cross_line_event_filter.eventFilter = resize_event_filter_fn.__get__(self.__resize_cross_line_event_filter)
         self.installEventFilter(self.__resize_cross_line_event_filter)
         
 
-        self.setAttribute(Qt.WA_TranslucentBackground, True) # 透明背景，必须和无边框结合使用
-        self.setWindowFlag(Qt.FramelessWindowHint, True) # 无边框
-        self.setWindowFlag(Qt.WindowStaysOnTopHint, True) # 置顶
+        self.__items: list[tuple[PopupItem, QRect]] = []
 
         self.__item_geometry_handler = PopupItemGeometryHandler()
+        for i in items:
+            self.add_item(*i)
 
-        self.__mask = QRegion()
-        self.editing_mode = False
-    
+        self.showMaximized()
+        self.hide()
+        
+    def resizeEvent(self, a0: QResizeEvent) -> None:
+        for item, relative_geo in self.__items:
+            item.unsetCursor()
+            item.setGeometry(self.__to_real_geo(relative_geo))
+        return super().resizeEvent(a0)
+        
     def paintEvent(self, a0: QPaintEvent) -> None:
         """
-        paint a half transparent background on editing mode, otherwise paint transparent background
+        paint a half transparent background for editing mode
         """
         p = QPainter(self)
         p.setPen(QColor(0,0,0,0))
-        if self.editing_mode:    
-            p.setBrush(QColor(33, 33, 33, 150))
-        else:
-            p.setBrush(QColor(0,0,0,0))
+        p.setBrush(QColor(33, 33, 33, 150))
         p.drawRect(self.rect())
         p.end()
 
-    def refresh_mask(self, recalculate: bool):
-        """
-        refresh mask, use mask on non editing mode, recalculate mask region if required
-        """
-        if self.editing_mode:
-            self.clearMask()
-            return
-        if not recalculate:
-            self.setMask(self.__mask)
-            return
-        res = QRegion()
-        for item, _ in self.__items:
-            res = res.united(item.geometry())
-        self.__mask = res
-        self.setMask(res)
-
-    # def resizeEvent(self, a0: QResizeEvent) -> None:
-    #     """
-    #     when me resized(which shouldn't happened) mask should be refreshed, and all items should be repaint
-    #     """
-    #     # for wrapper, relative_geo in self.__items:
-    #     #     print((relative_geo))
-    #     #     wrapper.setGeometry(self.__to_real_geo(relative_geo))
-    #     self.refresh_mask(True)
-    #     return super().resizeEvent(a0)
+    def __popup_item_menu(self, item: PopupItem):
+        # 获取菜单栏中的菜单项
+        menu = QMenu(self)
+        def delete_me():
+            self.remove_item(item.wrapped)
+            self.repaint()
+        menu.addAction('Delete', delete_me)
+        menu.exec_(item.mapToGlobal(item.rect().topRight()))
 
     def __on_item_event(self, item: PopupItem, event: QEvent):
-        # only do actions with
-        if not self.editing_mode:
+        # 如果是右键按下，唤起上下文菜单
+        if event.type() == QEvent.MouseButtonPress and event.button() == Qt.RightButton:
+            self.__popup_item_menu(item)
             return
-        self.__item_geometry_handler.handle(item, event)
-
-    def show(self) -> None:
-        self.__cross_line.raise_()
-        super().show()
         
-    def hide(self) -> None:
-        super().hide()
-
-    @property
-    def editing_mode(self):
-        return self.__editing_mode
-    
-    @editing_mode.setter
-    def editing_mode(self, new_editing_mode: bool):
-        if not new_editing_mode:
-            self.__item_geometry_handler.stop_editing()
+        # 无视鼠标左键以外的键按下和抬起
+        if not (event.type() in (QEvent.MouseButtonPress, QEvent.MouseButtonRelease) and event.button() != Qt.LeftButton):
+            self.__item_geometry_handler.handle(item, event)
+            return
         
-        self.__editing_mode = new_editing_mode
-        self.__cross_line.setVisible(new_editing_mode) 
-        
-        for item in self.__items:
-            item[0].set_interactive(not new_editing_mode)
-            item[0].unsetCursor()
-        
-        self.refresh_mask(True)
-
     def __to_real_geo(self, relative_geo: QRect):
         center = self.geometry().center()
         res = QRect(relative_geo)
         res.moveTopLeft(res.topLeft() + center)
+        print(self.geometry())
         return res
     
     def __to_relative_geo(self, real_geo: QRect):
@@ -223,9 +276,10 @@ class RealPopup(QWidget):
         wrapper.setParent(self)
         self.__cross_line.raise_()
         wrapper.installEventFilter(self.__delegate_event_filter)
-        self.__items.append((wrapper, relative_geo))
+        wrapper.interactive = False
         wrapper.setGeometry(self.__to_real_geo(relative_geo))
-        self.refresh_mask(True)
+        wrapper.show()
+        self.__items.append((wrapper, relative_geo))
 
     def remove_item(self, widget: QWidget):
         """
@@ -235,7 +289,55 @@ class RealPopup(QWidget):
         if wrapper:
             self.__items.remove(wrapper)
             wrapper[0].removeEventFilter(self.__delegate_event_filter)
-        self.refresh_mask(True)
+            wrapper[0].setParent(None)
+            wrapper[0].deleteLater()
+            return wrapper[0].wrapped
+
+    def wait_for_done(self) -> list[tuple[QWidget, QRect]] | None:
+        """
+        block until popup close, return (and move) widget and relative_geometry s
+        """
+        self.show()
+        loop = QEventLoop()
+        cancel_button = QPushButton(self)
+        apply_button = QPushButton(self)
+        size = self.size()
+
+        cancel_button_geo = QRect()
+        cancel_button_geo.setSize(QSize(200, 60))
+        cancel_button_geo.moveBottomRight(QPoint(size.width() - 600, size.height() - 100))
+        cancel_button.setGeometry(cancel_button_geo)
+        cancel_button.setText('Cancel')
+        cancel_button.show()
+
+        apply_button_geo = QRect()
+        apply_button_geo.setSize(QSize(200, 60))
+        apply_button_geo.moveBottomRight(QPoint(size.width() - 300, size.height() - 100))
+        apply_button.setGeometry(apply_button_geo)
+        apply_button.setText('Apply')
+        apply_button.show()
+
+        def cancel_me():
+            loop.exit(1)
+            cancel_button.deleteLater()
+            apply_button.deleteLater()
+        def apply_me():
+            loop.exit(0)
+            cancel_button.deleteLater()
+            apply_button.deleteLater()
+        cancel_button.clicked.connect(cancel_me)
+        apply_button.clicked.connect(apply_me)
+
+        ret = loop.exec()
+        self.hide()
+        if ret == 1:
+            return None
+        res = []
+        for item, _ in [*self.__items]:
+            relative_geo = self.__to_relative_geo(item.geometry())
+            res.append((item.wrapped, relative_geo))
+            self.remove_item(item.wrapped)
+        return res
 
 class PopupItemGeometryHandler:
     def __init__(self) -> None:
@@ -299,7 +401,6 @@ class PopupItemGeometryHandler:
         self.__editing_global_pos_start = None
         self.__editing_anchor = None
 
-
     def __handle_mouse_event(self, item: PopupItem, event: QEvent):
         # 不在编辑状态时，设置光标
         if not self.__editing_widget and event.type() in (QEvent.Leave, QEvent.MouseMove):
@@ -313,9 +414,8 @@ class PopupItemGeometryHandler:
         if self.__editing_widget is not None and self.__editing_widget is not item:
             return
 
-        # 如果是鼠标按下，设置状态，设置光标
+        # 如果是鼠标左键按下，设置状态，设置光标
         if event.type() == QEvent.MouseButtonPress:
-            print('按下')
             self.stop_editing()
             self.__setting_cursor(item, event)
             self.__editing_anchor = self.get_editing_ahchor(item.size(),event.pos())
@@ -326,14 +426,12 @@ class PopupItemGeometryHandler:
 
         if event.type() == QEvent.MouseButtonRelease:
             # 如果是鼠标抬起，重设状态，重设光标    
-            print('抬起')
             item.unsetCursor()
             self.stop_editing()
             return
 
         offset = event.globalPos() - self.__editing_global_pos_start
         geo = QRect(self.__editing_widget_original_geo)
-        print(self.__editing_anchor)
         # 鼠标移动事件，狠角色要来了
         # 如果是编辑位置，问题容易一些——获取当前鼠标位置相对于原位置的偏移量，并据此设置geometry
         # 编辑大小时，虽然让人感觉很虚，但是QRect里有很多方法让我不需要自己实现任何多余计算逻辑
@@ -364,17 +462,25 @@ class PopupItemGeometryHandler:
 if __name__ == '__main__':
     if QApplication.instance() is None:
         app = QApplication([])
+    else:
+        app = None
 
-    popup = RealPopup()
+    top = QPushButton()
+    top.setText('top')
+    top.clicked.connect(lambda: print('top'))
+    
+    bottom = QPushButton()
+    bottom.setText('bottom')
+    
+
+    popup = Popup([
+        (top, QRect(0,-100, 100,50)),
+        (bottom, QRect(0,100, 100,50))
+    ], under_cursor=True)
 
     manager_widget = QWidget()
     manager_widget.setWindowFlag(Qt.WindowStaysOnTopHint, True) # 置顶
     layout = QVBoxLayout(manager_widget)
-    toggle_editing_mode_checkbox = QCheckBox()
-    def go(v):
-        popup.editing_mode = v
-    toggle_editing_mode_checkbox.toggled.connect(go)
-    layout.addWidget(toggle_editing_mode_checkbox)
 
     toggle_show_btn = QPushButton()
     toggle_show_btn.setText('toggle')
@@ -388,28 +494,18 @@ if __name__ == '__main__':
     manager_widget.show()
 
     
-    top = PopupItem(QPushButton())
-    top.wrapped.setText('top')
-    popup.add_item(top, QRect(0,-100, 100,50))
 
     
-    
-    bottom = PopupItem(QPushButton())
-    bottom.wrapped.setText('bottom')
-    popup.add_item(bottom, QRect(0,100, 100,50))
-    
-    
-    left = PopupItem(QPushButton())
-    left.wrapped.setText('left')
-    popup.add_item(left, QRect(-200,0, 100,50))
+    # left = QPushButton()
+    # left.setText('left')
+    # popup.add_item(left, QRect(-200,0, 100,50))
 
     
-    right = PopupItem(QPushButton())
-    right.wrapped.setText('right')
-    popup.add_item(right, QRect(200,0, 100,50))
+    # right = QPushButton()
+    # right.setText('right')
+    # popup.add_item(right, QRect(200,0, 100,50))
 
-
-    popup.show()
-
-    if QApplication.instance() is app:
+    # popup.showMaximized()
+    # print(popup.wait_for_done())
+    if app is not None:
         app.exec()
